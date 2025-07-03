6912f21dd9ebe291afb6c13f73a40a9654227d93
Ali sadman Mashrafi <144376701+mashrafi05@users.noreply.github.com>
2025-07-03 16:54:17 +0600

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,21 @@
+
+package Array_Pack;
+
+import java.util.Scanner;
+
+public class passway {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        int Number1= input.nextInt();
+        
+        int Number2= input.nextInt();
+        
+        System.out.print(Number1+Number2);
+        
+        
+    }
+}
+
+
+
