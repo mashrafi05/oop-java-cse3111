fab71b3982da44a56d1285acfbb004bd27e4904f
Ali sadman Mashrafi <144376701+mashrafi05@users.noreply.github.com>
2025-07-03 16:53:54 +0600

    Create Character Input.java

---
+++ Character Input.java
@@ -0,0 +1,17 @@
+
+package Array_Pack;
+
+import java.util.Scanner;
+
+public class passway {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        char Number= input.next().charAt(0);
+        System.out.print(Number);
+        
+    }
+}
+
+
+
