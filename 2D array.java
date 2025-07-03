c541a4f902dfe16c9345be420a25268e85acec74
Ali sadman Mashrafi <144376701+mashrafi05@users.noreply.github.com>
2025-07-03 16:53:31 +0600

    Create 2D array.java

---
+++ 2D array.java
@@ -0,0 +1,16 @@
+
+package Array_Pack;
+
+public class passway {
+    public static void main(String[] args) {
+        int arr[][]={{1,2,3},{5,6,7}};
+        for(int i=0; i<arr.length;i++){
+            for(int j=0; j<arr[i].length;j++){
+                System.out.print(arr[i][j]);
+            }System.out.println();
+        }
+    }
+}
+
+
+
