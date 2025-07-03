009117a0d3c99ba8ed1c44afd846e6b477fdbb4d
Ali sadman Mashrafi <144376701+mashrafi05@users.noreply.github.com>
2025-07-03 16:55:11 +0600

    Create object initialize By reference variable .java

---
+++ object initialize By reference variable .java
@@ -0,0 +1,28 @@
+
+package Pack;
+import java.util.Scanner;
+
+public class Mash {
+	
+	int id;
+	String name;
+	public static void main (String args[]) {
+		
+		Mash obj= new Mash();
+		Mash obj1= new Mash();
+		
+		obj.id=5;
+		obj.name="Mashrafi";
+
+		
+		
+		
+		
+		System.out.println(obj.id);
+		System.out.print(obj.name);
+		
+		
+		
+	}
+
+}
