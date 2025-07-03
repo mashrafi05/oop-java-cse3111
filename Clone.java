64090347ee4a6ff66183017598a95eb07cbb8c68
Ali sadman Mashrafi <144376701+mashrafi05@users.noreply.github.com>
2025-07-03 16:52:51 +0600

    Create Clone.java

---
+++ Clone.java
@@ -0,0 +1,18 @@
+
+package Array_Pack;
+
+public class passway {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7,8,9,10};
+        max(arr);
+    }
+    static void max(int arr[]) {
+    
+        int srr[]= arr.clone();
+        for(int i:srr){
+            System.out.print(i);
+        }
+}
+}
+
+
